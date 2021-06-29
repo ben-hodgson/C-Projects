80ba5ceb6baf46f36fa45bfe007eb8317564ddd4
ben-hodgson <bthodgson@gmail.com>
2021-06-29 20:08:45 +0100

    Hello_World for all three

---
+++ C# Projects/Hello_World.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program {
+   class MyApplication {
+      static void Main(string[] args) {
+         Console.WriteLine("Hello World");
+         Console.Read();
+      }
+   }
+}
